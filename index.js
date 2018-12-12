b2c464050eba938296617678eee055636c7ce1c8
Travo100 <travo100@gmail.com>
2018-12-11 17:20:30 -0800

    Added startup code

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var cTable = require("console.table");
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : process.env.DBPASSWORD,
+  database : 'schoolDB'
+});
+ 
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log(`Connection id is ${connection.threadId}`);
+});
+ 
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+ 
+// connection.end();
---
+++ schema.sql
@@ -0,0 +1,60 @@
+DROP DATABASE IF EXISTS schoolDB;
+
+-- CREATE A DATABASE schoolDB
+CREATE DATABASE schoolDB;
+
+-- USE a database 
+USE schoolDB;
+
+-- CREATE a TABLE called classroom
+CREATE TABLE classroom (
+	-- id 
+    id INT AUTO_INCREMENT NOT NULL,
+	-- name 
+    name VARCHAR(255) NOT NULL,
+    -- room
+    room INTEGER(4) NOT NULL,
+    
+    PRIMARY KEY(id)
+);
+
+INSERT INTO classroom(name, room) VALUES ("Coding Boot Camp", 308);
+SELECT * FROM classroom;
+
+-- CREATE a TABLE called student
+CREATE TABLE student ( 
+	-- id
+    id INT AUTO_INCREMENT NOT NULL,
+	-- name
+    name VARCHAR(255) NOT NULL,
+    -- classRoomTheyAreInId,
+    classRoomId INTEGER(4) NOT NULL,
+    -- GPA 3.5 2.25 4.0 9.99
+    gpa DECIMAL(3, 2) NOT NULL,
+	-- favoriteSubject
+    favSubject VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO student(name, classRoomId, gpa, favSubject) VALUES ("Cat", 1, 3.75, "JavaScript"), ("Taylor", 1, 3.5, "Music"), ("Saba", 2, 4.00, "Node");
+INSERT INTO student(name, classRoomId, gpa, favSubject) VALUES ("Iris", 3, 3.88, "VB.net");
+
+SELECT * FROM student;
+
+-- DELETE FROM student WHERE id = 3;
+
+-- INSERT some records into said table 
+
+-- SELECT the table
+
+SELECT classroom.name, classroom.room, student.name, student.favSubject, student.gpa
+FROM classroom 
+INNER JOIN student ON classroom.id = student.classRoomId;
+
+SELECT classroom.name, classroom.room, student.name, student.favSubject, student.gpa
+FROM classroom 
+LEFT JOIN student ON classroom.id = student.classRoomId;
+
+SELECT classroom.name, classroom.room, student.name, student.favSubject, student.gpa
+FROM classroom 
+RIGHT JOIN student ON classroom.id = student.classRoomId;
